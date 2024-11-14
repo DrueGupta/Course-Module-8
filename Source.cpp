55c47f71a6e02a2e4d2c5b4de3e8d6c0799b1a37
Drue Gupta <1031762@my.scccd.edu>
2024-11-14 10:28:17 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
 #include "Header.h"
+#include "Header1.h"
+#include "Header2.h"
 using namespace std;
 
 int main()
 {
-	Taxicab a("Sam", 0, 0);
-	a.moveRight(13);
-	a.moveLeft(8);
-	a.moveUp(6);
-	a.moveDown(0);
+	TaxiCompany PickmeUp;
+	PickmeUp.addTaxi("Big Slappy", 10.00);
+	PickmeUp.addTaxi("Ambatakam", 5.00);
+	PickmeUp.addTaxi("King", 200.00);
+	PickmeUp.orderTaxi(5, 8, 12, 9);
+	PickmeUp.orderTaxi(- 2, - 8, 2, 2);
 }
